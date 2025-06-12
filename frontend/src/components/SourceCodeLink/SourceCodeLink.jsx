db39aec88640e21044ce6873da29f111d61994dc
6ebeng <shebeng.devs@gmail.com>
2025-06-12 16:06:28 +0300

    fix: update repository links and homepage in package.json; format code in SourceCodeLink component

--- frontend/src/components/SourceCodeLink/SourceCodeLink.jsx
+++ frontend/src/components/SourceCodeLink/SourceCodeLink.jsx
@@ -1,18 +1,15 @@
 function SourceCodeLink({ left, right }) {
-  const position = left ? "left" : right ? "right" : "";
+	const position = left ? 'left' : right ? 'right' : '';
 
-  return (
-    <ul className={`nav navbar-nav pull-xs-${position}`}>
-      <li className="nav-item">
-        <a
-          className="nav-link"
-          href="https://github.com/Yassin-H-Rassul/ukh-blog-website"
-        >
-          <i className="ion-social-github"></i> Source code
-        </a>
-      </li>
-    </ul>
-  );
+	return (
+		<ul className={`nav navbar-nav pull-xs-${position}`}>
+			<li className="nav-item">
+				<a className="nav-link" href="https://github.com/6ebeng/blog-app">
+					<i className="ion-social-github"></i> Source code
+				</a>
+			</li>
+		</ul>
+	);
 }
 
 export default SourceCodeLink;
